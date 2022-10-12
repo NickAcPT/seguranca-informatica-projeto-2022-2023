52079081d03d465371235b6b92ad1fef73c7652d
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-10-12 19:01:00 +0100

    Simplify stuff

--- vending-app/src/main/java/pt/ua/segurancainformatica/app/vending/Entrypoint.java
+++ vending-app/src/main/java/pt/ua/segurancainformatica/app/vending/Entrypoint.java
@@ -3,6 +3,7 @@ package pt.ua.segurancainformatica.app.vending;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import org.jetbrains.annotations.Nullable;
 
 public class Entrypoint extends Application {
 
@@ -10,15 +11,21 @@ public class Entrypoint extends Application {
         launch();
     }
 
+    @Nullable
+    private static Stage primaryStage = null;
+
+    public static void loadFile(String fxml) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(Entrypoint.class.getResource("/" + fxml));
+        var scene = new javafx.scene.Scene(fxmlLoader.load());
+        if (primaryStage != null) {
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Load Teste.fxml
-        FXMLLoader fxmlLoader = new FXMLLoader(Entrypoint.class.getResource("/Teste.fxml"));
-        // Create the scene
-        var scene = new javafx.scene.Scene(fxmlLoader.load(), 300, 275);
-        // Set the scene
-        primaryStage.setScene(scene);
-        // Show the stage
-        primaryStage.show();
+        Entrypoint.primaryStage = primaryStage;
+        loadFile("Teste.fxml");
     }
 }
