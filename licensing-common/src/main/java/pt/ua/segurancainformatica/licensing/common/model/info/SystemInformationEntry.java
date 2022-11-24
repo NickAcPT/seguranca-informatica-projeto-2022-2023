e621000f61f4e6438363d5c916ac3462f3cd5056
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-11-24 16:59:09 +0000

    Remove comment

--- licensing-common/src/main/java/pt/ua/segurancainformatica/licensing/common/model/info/SystemInformationEntry.java
+++ licensing-common/src/main/java/pt/ua/segurancainformatica/licensing/common/model/info/SystemInformationEntry.java
@@ -6,7 +6,6 @@ package pt.ua.segurancainformatica.licensing.common.model.info;
  * A license gets generated for a specific system, and then the system information is used to verify if the
  * license is valid for the system.
  */
-// todo: add json serialization
 public interface SystemInformationEntry<T> {
     /**
      * Whether the information stored in this entry matches the information computed from data in this computer.
