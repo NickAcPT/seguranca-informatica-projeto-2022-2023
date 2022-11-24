73e22a13b94d5cf5315eb9101202ca8a9daf52ce
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-11-24 13:40:49 +0000

    Add Smile dataformat

--- licensing-lib/build.gradle.kts
+++ licensing-lib/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":citizen-card-lib"))
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-smile:2.14.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
