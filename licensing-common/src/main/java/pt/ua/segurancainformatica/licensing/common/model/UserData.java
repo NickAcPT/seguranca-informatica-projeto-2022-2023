e62100073813e1797114ac191f22d6821ad2b7f0
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-11-26 16:32:21 +0000

    Add way to read encoded public key

--- licensing-common/src/main/java/pt/ua/segurancainformatica/licensing/common/model/UserData.java
+++ licensing-common/src/main/java/pt/ua/segurancainformatica/licensing/common/model/UserData.java
@@ -1,5 +1,25 @@
 package pt.ua.segurancainformatica.licensing.common.model;
 
-public record UserData() {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.jetbrains.annotations.Nullable;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+public record UserData(
+        String fullName,
+        byte[] encodedPublicKey) {
+
+    @JsonIgnore
+    public @Nullable PublicKey publicKey() throws InvalidKeySpecException {
+        var spec = new X509EncodedKeySpec(encodedPublicKey);
+        try {
+            return KeyFactory.getInstance("RSA").generatePublic(spec);
+        } catch (NoSuchAlgorithmException e) { // wat
+            throw new RuntimeException(e);
+        }
+    }
 }
