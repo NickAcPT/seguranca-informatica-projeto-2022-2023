e6210001ea5edf342f9364733b81d7c15f362b91
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-12-05 18:44:51 +0000

    Sort Enum

--- licensing-lib/src/main/java/pt/ua/segurancainformatica/licensing/common/wrapper/SecureWrapperInvalidatedReason.java
+++ licensing-lib/src/main/java/pt/ua/segurancainformatica/licensing/common/wrapper/SecureWrapperInvalidatedReason.java
@@ -1,21 +1,21 @@
 package pt.ua.segurancainformatica.licensing.common.wrapper;
 
 public enum SecureWrapperInvalidatedReason {
-    NULL_CIPHER_KEY("Cipher key is null."),
-    SYMMETRIC_KEY_CIPHERED_WITH_IV("Symmetric key ciphered with an IV."),
-    ERROR_CIPHERING_SYMMETRIC_KEY("Error ciphering symmetric key."),
-    ERROR_DECIPHERING_SYMMETRIC_KEY("Error deciphering symmetric key."),
-    MANAGER_PRIVATE_KEY_MISSING("Unable to decipher the symmetric key because the manager private key is missing."),
     ERROR_CIPHERING_SECURE_OBJECT("Error ciphering secure object."),
+    ERROR_CIPHERING_SYMMETRIC_KEY("Error ciphering symmetric key."),
     ERROR_DECIPHERING_SECURE_OBJECT("Error deciphering secure object."),
-    USER_KEYPAIR_MISSING("User key pair is null."),
-    ERROR_SIGNING_SECURE_OBJECT("Error signing secure object."),
-    OBJECT_HAS_NO_SIGNATURE("Object has no signature."),
-    INVALID_OBJECT_SIGNATURE("Object signature is invalid."),
-    PUBLIC_KEY_MISMATCH("Public key mismatch: Known public key is different from the one used to sign the object."),
-    ERROR_VERIFYING_SIGNATURE("Error verifying signature."),
-    ERROR_SERIALIZE_OBJECT("Error serializing object."),
+    ERROR_DECIPHERING_SYMMETRIC_KEY("Error deciphering symmetric key."),
     ERROR_DESERIALIZE_OBJECT("Error deserializing object: Unable to deserialize the object from smile format."),
+    ERROR_SERIALIZE_OBJECT("Error serializing object."),
+    ERROR_SIGNING_SECURE_OBJECT("Error signing secure object."),
+    ERROR_VERIFYING_SIGNATURE("Error verifying signature."),
+    INVALID_OBJECT_SIGNATURE("Object signature is invalid."),
+    MANAGER_PRIVATE_KEY_MISSING("Unable to decipher the symmetric key because the manager private key is missing."),
+    NULL_CIPHER_KEY("Cipher key is null."),
+    OBJECT_HAS_NO_SIGNATURE("Object has no signature."),
+    PUBLIC_KEY_MISMATCH("Public key mismatch: Known public key is different from the one used to sign the object."),
+    SYMMETRIC_KEY_CIPHERED_WITH_IV("Symmetric key ciphered with an IV."),
+    USER_KEYPAIR_MISSING("User key pair is null."),
     ;
 
     private final String message;
