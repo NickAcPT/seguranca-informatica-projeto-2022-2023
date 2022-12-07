e621000a0e2ac0c06980bece07dbf0b920712f90
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-12-07 18:24:24 +0000

    Add shadow jar

--- vending-app/build.gradle.kts
+++ vending-app/build.gradle.kts
@@ -2,6 +2,7 @@
     java
     application
     id("org.openjfx.javafxplugin") version "0.0.13"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 javafx {
@@ -10,6 +11,7 @@ javafx {
 }
 
 dependencies {
+    implementation(project(":licensing-lib"))
     implementation("org.jfxtras:jmetro:11.6.16")
     implementation("org.apache.commons:commons-csv:1.9.0")
 }
