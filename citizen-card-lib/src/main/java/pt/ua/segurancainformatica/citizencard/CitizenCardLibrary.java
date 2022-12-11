e621000a5767a3f2d51b0b89136d22e4923ac7de
NickAc <32451103+NickAcPT@users.noreply.github.com>
2022-12-11 20:08:02 +0000

    Revert "Add way to mock a citizen card because I am sick and tired of this fucking piece of shit software                           				   	 	 	 	  	 	 		        	                "
    This reverts commit e62100062344801a3f91b5f99598ca963f3e0b8c.

--- citizen-card-lib/src/main/java/pt/ua/segurancainformatica/citizencard/CitizenCardLibrary.java
+++ citizen-card-lib/src/main/java/pt/ua/segurancainformatica/citizencard/CitizenCardLibrary.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import pt.ua.segurancainformatica.citizencard.callbacks.CitizenCardListener;
 import pt.ua.segurancainformatica.citizencard.impl.CitizenCardLibraryImpl;
-import pt.ua.segurancainformatica.citizencard.mock.CitizenCardLibraryMock;
 import pt.ua.segurancainformatica.citizencard.model.CitizenCard;
 
 import java.security.Provider;
@@ -23,9 +22,6 @@ public interface CitizenCardLibrary extends AutoCloseable{
      */
     @NotNull
     static CitizenCardLibrary citizenCardLibrary() {
-        if (System.getProperty("MOCK_CITIZEN_CARD", "false").equals("true")) {
-            return new CitizenCardLibraryMock(System.getProperty("HAS_MOCK_CARD", "false").equals("true"));
-        }
         return new CitizenCardLibraryImpl();
     }
 
